9cadb78f7ccef3b627a4256957198e4e32f108cb
andresws12 <andreshernandez12.1995@gmail.com>
2023-07-13 17:59:57 +0200

    Fix duplciate color

--- src/models/AppColors.ts
+++ src/models/AppColors.ts
@@ -5,7 +5,7 @@ export enum AppColors {
   MAIN_COLOR_MEDIUM_LIGHT = '#ccc',
   MAIN_COLOR_MEDIUM_DARK = '#2b5973',
   MAIN_COLOR_SUCCESS = '#12fd63',
-  MAIN_COLOR_FAIL = '#e01f2f',
+  MAIN_COLOR_FAIL = '#e01f2r',
 }
 
 export enum AppColorsLigther {
