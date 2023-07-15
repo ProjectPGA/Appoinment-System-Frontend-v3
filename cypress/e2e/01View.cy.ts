9f813cfe5951f3c8f96af787803aa1421c56da9b
gonzalo-plaza <plazaruedag@gmail.com>
2023-07-15 13:40:01 +0200

    Cypress Pipeline: Comment other it´s

--- cypress/e2e/01View.cy.ts
+++ cypress/e2e/01View.cy.ts
@@ -10,32 +10,32 @@ describe('01 View', () => {
   it('Show BASE_URL value', () => {
     cy.log(loginFunctions.BASE_URL);
   });
-  it('01 Login with wrong credentials and failure login', () => {
-    cy.get(loginSelectors.emailInputLoginPage).type(
-      loginCredentials.USER_TEST_EMAIL
-    );
-    cy.get(loginSelectors.passwordInputLoginPage).type(
-      loginCredentials.USER_TEST_WRONG_PASSWORD
-    );
-    cy.get(loginSelectors.submitLoginPage).click();
-    loginFunctions
-      .waitForLoginStatusChange()
-      .then((finalLoginStatus: string) => {
-        expect(finalLoginStatus).to.deep.equal(RequestStatus.FAILURE);
-      });
-  });
-  it('01 Login with correct credentials and success login', () => {
-    cy.get(loginSelectors.emailInputLoginPage).type(
-      loginCredentials.USER_TEST_EMAIL
-    );
-    cy.get(loginSelectors.passwordInputLoginPage).type(
-      loginCredentials.USER_TEST_PASSWORD
-    );
-    cy.get(loginSelectors.submitLoginPage).click();
-    loginFunctions
-      .waitForLoginStatusChange()
-      .then((finalLoginStatus: string) => {
-        expect(finalLoginStatus).to.deep.equal(RequestStatus.SUCCESS);
-      });
-  });
+  // it('01 Login with wrong credentials and failure login', () => {
+  //   cy.get(loginSelectors.emailInputLoginPage).type(
+  //     loginCredentials.USER_TEST_EMAIL
+  //   );
+  //   cy.get(loginSelectors.passwordInputLoginPage).type(
+  //     loginCredentials.USER_TEST_WRONG_PASSWORD
+  //   );
+  //   cy.get(loginSelectors.submitLoginPage).click();
+  //   loginFunctions
+  //     .waitForLoginStatusChange()
+  //     .then((finalLoginStatus: string) => {
+  //       expect(finalLoginStatus).to.deep.equal(RequestStatus.FAILURE);
+  //     });
+  // });
+  // it('01 Login with correct credentials and success login', () => {
+  //   cy.get(loginSelectors.emailInputLoginPage).type(
+  //     loginCredentials.USER_TEST_EMAIL
+  //   );
+  //   cy.get(loginSelectors.passwordInputLoginPage).type(
+  //     loginCredentials.USER_TEST_PASSWORD
+  //   );
+  //   cy.get(loginSelectors.submitLoginPage).click();
+  //   loginFunctions
+  //     .waitForLoginStatusChange()
+  //     .then((finalLoginStatus: string) => {
+  //       expect(finalLoginStatus).to.deep.equal(RequestStatus.SUCCESS);
+  //     });
+  // });
 });
