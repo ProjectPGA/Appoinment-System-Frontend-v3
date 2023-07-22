c613896663d544d90ffb248218871aaff76dc2ab
gonzalo-plaza <plazaruedag@gmail.com>
2023-07-22 19:14:37 +0200

    [AS-34] Jest unit test: Updating JSDoc to the changeLanguage function

--- src/utils/functionUtils/localizationFunctions.ts
+++ src/utils/functionUtils/localizationFunctions.ts
@@ -4,8 +4,7 @@ import { Locales } from '@/localization/locales';
 /**
  * The `changeLanguage()` function is toggling between two language locales (Spanish and English) using
  * the `vue-i18n` library. It checks the current locale value and if it's equal to `Locales.ES`
- * (Spanish), it sets the locale value to `Locales.EN` (English), and vice versa. The `:click`
- * directive in the button element is calling this function when the button is clicked.
+ * (Spanish), it sets the locale value to `Locales.EN` (English), and vice versa.
  */
 export function changeLanguage(): void {
   i18nGlobal.locale === Locales.ES
