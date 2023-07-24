b94be787d432ffeee192ff0d396f69ac7143d02c
gonzalo-plaza <plazaruedag@gmail.com>
2023-07-24 19:06:05 +0200

    [AS-34] Jest unit test: Fix 'en' path to get 'en' content

--- src/localization/locales/messages.ts
+++ src/localization/locales/messages.ts
@@ -1,7 +1,7 @@
 import { Locales } from '@/localization/locales/locales';
 
 import es from '@/localization/locales/es/main';
-import en from '@/localization/locales/es/main';
+import en from '@/localization/locales/en/main';
 
 export const messages = {
   [Locales.ES]: es,
