474f72ada8a252e781ff2fd3ab9397b76e18b8a9
gonzalo-plaza <plazaruedag@gmail.com>
2023-09-09 21:47:29 +0200

    [AS-34] Jest unit tests: Adding test to BASE_URL and ApiPrefix

--- src/webservices/ApiPrefix.spec.ts
+++
@@ -1,9 +0,0 @@
-import { BASE_URL, apiPrefix } from './consts';
-
-describe('01 ApiPrefix', () => {
-  it('01 - 1 Check that ApiPrefix mount url correctly', () => {
-    const completeUrl: string = apiPrefix('/mockPath');
-
-    expect(completeUrl).toBe(BASE_URL + '/mockPath');
-  });
-});
---
+++ src/webservices/consts.spec.ts
@@ -0,0 +1,26 @@
+import { BASE_URL, apiPrefix } from './consts';
+
+describe('01 BASE_URL const', () => {
+  // TODO: Check why 'test:coverage' does not detect that we cover the two possible cases in this test
+  it('01 - 1 Should be the value of VITE_API_URL', () => {
+    expect(BASE_URL).toBe(import.meta.env.VITE_API_URL);
+  });
+
+  it('01 - 1 Should be empty when the value of VITE_API_URL is not valid', () => {
+    const originalEnv = import.meta.env.VITE_API_URL;
+
+    import.meta.env.VITE_API_URL = null;
+
+    expect(BASE_URL).toBe('');
+
+    import.meta.env.VITE_API_URL = originalEnv;
+  });
+});
+
+describe('02 ApiPrefix', () => {
+  it('02 - 1 Check that ApiPrefix mount url correctly', () => {
+    const COMPLETE_URL: string = apiPrefix('/mockPath');
+
+    expect(COMPLETE_URL).toBe(BASE_URL + '/mockPath');
+  });
+});
