11d2e600575eb89d8805b0ab7844ee6d40afc014
gonzalo-plaza <plazaruedag@gmail.com>
2023-09-23 15:45:21 +0200

    [AS-49] Remove todo

--- src/models/user/UserAuthData.ts
+++ src/models/user/UserAuthData.ts
@@ -1,6 +1,5 @@
 import { User } from './User';
 
-// TODO: Rename the UserAuthData interface to a more appropriate name and check the impact
 export interface UserAuthData {
   user: User | null;
   accessToken: string | null;
