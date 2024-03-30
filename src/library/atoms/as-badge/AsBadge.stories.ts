45222f6223eb3a96475785cd5290a5f40d97c781
gonzalo-plaza <plazaruedag@gmail.com>
2024-03-30 17:57:26 +0100

    Design system: Rename parent folder of all components in storybook

--- src/library/atoms/as-badge/AsBadge.stories.ts
+++ src/library/atoms/as-badge/AsBadge.stories.ts
@@ -5,7 +5,7 @@ import { boolean } from 'yup';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
-  title: 'AsBadge',
+  title: 'Appointment System/AsBadge',
   component: AsBadge,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs'],
--- src/library/atoms/as-input/AsInput.stories.ts
+++ src/library/atoms/as-input/AsInput.stories.ts
@@ -4,7 +4,7 @@ import AsInput from './AsInput.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
-  title: 'Example/AsInput',
+  title: 'Appointment System/AsInput',
   component: AsInput,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs'],
