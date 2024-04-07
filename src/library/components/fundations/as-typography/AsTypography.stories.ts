d94cdcb36422c3d100310ff630e026336b93ec01
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-07 11:07:56 +0200

    [AS-64] Add AsTypography story

---
+++ src/library/components/fundations/as-typography/AsTypography.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import AsTypographyTemplate from './template/AsTypographyTemplate.vue';
+
+const meta: Meta<typeof AsTypographyTemplate> = {
+  title: 'Fundations/Typography',
+  component: AsTypographyTemplate,
+  argTypes: {
+    head: {
+      control: 'select',
+      options: ['heading', 'body', 'button'],
+    },
+    size: {
+      control: 'select',
+      options: ['extralarge', 'large', 'medium', 'small'],
+    },
+    weight: {
+      control: 'select',
+      options: ['regular', 'semi', 'bold'],
+    },
+  },
+};
+
+//👇 This default export determines where your story goes in the story list
+export default meta;
+type Story = StoryObj<typeof AsTypographyTemplate>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Typography: Story = {
+  render: args => ({
+    components: { AsTypographyTemplate },
+    setup() {
+      return { args };
+    },
+    template: '<AsTypographyTemplate :args="args" />',
+  }),
+  args: {
+    head: 'body',
+    size: 'large',
+    weight: 'regular',
+  },
+};
