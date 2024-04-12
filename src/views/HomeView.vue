92132f1cebe5fb9c322538293f93a5ae352275bf
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-12 20:41:54 +0200

    [AS-67] HomeView: Add create-user-form component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,47 +24,7 @@
       <h2 class="home-view__title">
         {{ $t('views.home.titles.createUserTitle') }}
       </h2>
-      <as-card>
-        <form class="create-user-form">
-          <div class="create-user-form__inputs-container">
-            <as-input
-              :label="$t('views.home.createUser.name.label')"
-              :placeholder="$t('views.home.createUser.name.placeholder')"
-            />
-            <as-input
-              :label="$t('views.home.createUser.surname.label')"
-              :placeholder="$t('views.home.createUser.surname.placeholder')"
-            />
-          </div>
-          <div class="create-user-form__inputs-container">
-            <as-input
-              :label="$t('views.home.createUser.email.label')"
-              :placeholder="$t('views.home.createUser.email.placeholder')"
-            />
-            <as-input
-              :label="$t('views.home.createUser.roles.label')"
-              :placeholder="$t('views.home.createUser.roles.placeholder')"
-            />
-          </div>
-          <div class="create-user-form__inputs-container">
-            <as-input
-              type="password"
-              :label="$t('views.home.createUser.password.label')"
-              :placeholder="$t('views.home.createUser.password.placeholder')"
-            />
-            <as-input
-              type="password"
-              :label="$t('views.home.createUser.repeatPassword.label')"
-              :placeholder="
-                $t('views.home.createUser.repeatPassword.placeholder')
-              "
-            />
-          </div>
-          <as-button>
-            {{ $t('views.home.createUser.enterButton') }}
-          </as-button>
-        </form>
-      </as-card>
+      <create-user-form />
     </div>
   </section>
   <section class="home-view__section">
@@ -77,9 +37,8 @@
 import ButtonTranslation from '@/components/common/ButtonTranslation.vue';
 import UserData from '@/components/common/UserData.vue';
 import AllUsers from '@/components/home/AllUsers.vue';
+import CreateUserForm from '@/components/home/CreateUserForm.vue';
 
-import AsCard from '@/library/components/atoms/as-card/AsCard.vue';
-import AsInput from '@/library/components/atoms/as-input/AsInput.vue';
 import AsButton from '@/library/components/atoms/as-button/AsButton.vue';
 
 import { useAuthStore } from '@/stores/auth';
@@ -107,14 +66,6 @@ const logout = () => {
 
   &__button {
     margin-bottom: 12px;
-  }
-}
-
-.create-user-form {
-  &__inputs-container {
-    display: flex;
-    gap: 16px;
-    margin-bottom: 10px;
   }
 }
 </style>
