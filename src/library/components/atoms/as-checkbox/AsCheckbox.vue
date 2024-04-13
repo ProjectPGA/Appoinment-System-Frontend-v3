76e6b8d46111cdfeb592081bf6bbb2520d9303be
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-13 18:03:38 +0200

    [AS-67] AsCheckbox.vue: Create the basic component

--- src/library/components/atoms/as-checkbox/AsCheckbox.vue
+++ src/library/components/atoms/as-checkbox/AsCheckbox.vue
@@ -1,14 +1,18 @@
 <template>
-  <as-input
-    :model-value="value"
-    :checked="checked"
-    type="checkbox"
-    @change="handleChange"
-  />
+  <label class="as-checkbox__label" :for="checkboxId"
+    ><input
+      :id="checkboxId"
+      class="as-checkbox__input"
+      :checked="checked"
+      :value="checkedValue"
+      :name="name"
+      type="checkbox"
+      @change="handleChange"
+    />{{ label }}</label
+  >
 </template>
 <script lang="ts" setup>
-import AsInput from '@/library/components/atoms/as-input/AsInput.vue';
-
+import './as-checkbox.scss';
 import { useField } from 'vee-validate';
 
 const props = defineProps({
@@ -16,7 +20,15 @@ const props = defineProps({
     type: String,
     required: true,
   },
-  value: {
+  checkedValue: {
+    type: String,
+    required: true,
+  },
+  checkboxId: {
+    type: String,
+    required: true,
+  },
+  label: {
     type: String,
     required: true,
   },
@@ -25,6 +37,6 @@ const props = defineProps({
 // If the name changes you want `useField` to be able to pick it up
 const { handleChange, checked } = useField(() => props.name, undefined, {
   type: 'checkbox',
-  checkedValue: props.value,
+  checkedValue: props.checkedValue,
 });
 </script>
