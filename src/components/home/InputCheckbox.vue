26ab7543d6e2efaeb9e4168152a2307a418094e2
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-13 14:30:50 +0200

    [AS-67] Create InputChecbox component

---
+++ src/components/home/InputCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <as-input
+    :model-value="value"
+    :checked="checked"
+    type="checkbox"
+    @change="handleChange"
+  />
+</template>
+<script lang="ts" setup>
+import AsInput from '@/library/components/atoms/as-input/AsInput.vue';
+
+import { useField } from 'vee-validate';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+// The `name` is returned in a function because we want to make sure it stays reactive
+// If the name changes you want `useField` to be able to pick it up
+const { handleChange, checked } = useField(() => props.name, undefined, {
+  type: 'checkbox',
+  checkedValue: props.value,
+});
+</script>
