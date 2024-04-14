048ece68265bb2cee44a1297adaff3dc552cd492
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-14 14:13:50 +0200

    [AS-67] Auth Store tests: Add register tests

--- src/stores/authStore.spec.ts
+++ src/stores/authStore.spec.ts
@@ -7,6 +7,7 @@ import { createRandomUser } from '@/utils/mocks/user/mockUser';
 import * as AuthWebservice from '@/webservices/AuthWebservice';
 import { createRandomUserAuthData } from '@/utils/mocks/user/mockUserAuthData';
 import { createRandomUsersList } from '@/utils/mocks/user/mockUsers';
+import { RegisterUserResponse } from '@/models/auth/registerUser';
 
 jest.mock('@/webservices/AuthWebservice');
 jest.mock('vue-i18n', () => ({
@@ -23,6 +24,7 @@ jest.mock('vue-router', () => ({
 const loginServiceMock = jest.spyOn(AuthWebservice, 'loginService');
 const logoutServiceMock = jest.spyOn(AuthWebservice, 'logoutService');
 const getAllUsersServiceMock = jest.spyOn(AuthWebservice, 'getAllUsersService');
+const registerServiceMock = jest.spyOn(AuthWebservice, 'registerService');
 
 const mockUserAuthData = createRandomUserAuthData();
 const mockUserLoginValue = createRandomUser();
@@ -270,5 +272,72 @@ describe('06 Auth store: logout', () => {
     expect(authStore.isLogged).toBe(false);
     expect(authStore.isLoading).toBe(false);
     expect(authStore.userAuthData).toBeNull();
+  });
+});
+
+describe('07 Auth store: register', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('06 - 01 Should register', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const authStore = useAuthStore(pinia);
+    const mockUser = createRandomUser();
+
+    registerServiceMock.mockResolvedValue(mockUser);
+
+    const expectedResponse = {
+      error: false,
+      result: mockUser,
+    };
+
+    const response: RegisterUserResponse = await authStore.register(mockUser);
+
+    expect(response).toStrictEqual(expectedResponse);
+  });
+
+  it('06 - 01 Should handle network error', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const authStore = useAuthStore(pinia);
+    const mockUser = createRandomUser();
+
+    registerServiceMock.mockRejectedValue(new Error('Network error'));
+
+    const expectedResponse = {
+      error: true,
+    };
+
+    const response: RegisterUserResponse = await authStore.register(mockUser);
+
+    expect(response).toStrictEqual(expectedResponse);
+  });
+
+  it('06 - 01 Should handle 500 Internal Server Error', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const authStore = useAuthStore(pinia);
+    const mockUser = createRandomUser();
+
+    registerServiceMock.mockRejectedValue({
+      isAxiosError: true,
+      response: { status: 500 },
+    });
+
+    const expectedResponse = {
+      error: true,
+      status: 500,
+    };
+
+    const response: RegisterUserResponse = await authStore.register(mockUser);
+
+    expect(response).toStrictEqual(expectedResponse);
   });
 });
