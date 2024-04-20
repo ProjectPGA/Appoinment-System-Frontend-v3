f23c3ba38e0c3918d5af9444a63aa680db242323
gonzalo-plaza <plazaruedag@gmail.com>
2024-04-20 19:24:11 +0200

    [AS-67] registerUser: Create RegisterUserRequest interface

--- src/models/auth/registerUser.ts
+++ src/models/auth/registerUser.ts
@@ -1,7 +1,10 @@
 import { UserAuthData } from '@/models/user/UserAuthData';
+import { User } from '../user/User';
 
 export interface RegisterUserResponse {
   error: boolean;
   result?: UserAuthData;
   status?: number;
 }
+
+export interface RegisterUserRequest extends Omit<User, '_id'> {}
