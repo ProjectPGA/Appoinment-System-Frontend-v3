c8b9c622147eb733ca1863745803befcdec069eb
gonzalo-plaza <plazaruedag@gmail.com>
2024-05-04 20:28:00 +0200

    [AS-83] Users Store: Create users store

---
+++ src/stores/users.ts
@@ -0,0 +1,112 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+import { UserAuthData } from '@/models/user/UserAuthData';
+
+import {
+  RegisterUserRequest,
+  RegisterUserResponse,
+} from '@/models/auth/registerUser';
+
+import axios from 'axios';
+
+import {
+  deleteUserService,
+  getAllUsersService,
+  registerService,
+} from '@/webservices/users/UsersWebservice';
+
+export const useUsersStore = defineStore('auth', () => {
+  const userAuthData = ref<UserAuthData | null>(null);
+  const users = ref<UserAuthData[] | null>([]);
+  const isLoading = ref<boolean>(false);
+
+  // Common private Methods
+  // GLOBAL METHODS
+
+  /**
+   * The `register` function in TypeScript handles registration requests asynchronously, returning a
+   * response object indicating success or failure.
+   * @param {User} registerData - Is of type `User`. It contains the data
+   * needed to register a user, such as username, email, password, etc.
+   * @returns Returns a Promise that resolves to a `RegisterUserResponse`
+   * object. The `RegisterUserResponse` object contains an `error` property indicating if an error
+   * occurred during the registration process, and a `result` property that holds either the successful
+   * result of the registration or the error response in case of failure.
+   */
+  const register = async (
+    registerData: RegisterUserRequest
+  ): Promise<RegisterUserResponse> => {
+    try {
+      const response: UserAuthData = await registerService(registerData);
+
+      const successResult: RegisterUserResponse = {
+        error: false,
+        result: response,
+      };
+
+      return successResult;
+    } catch (error) {
+      console.error(error);
+
+      if (axios.isAxiosError(error)) {
+        const axiosErrorResult: RegisterUserResponse = {
+          error: true,
+          status: error.response?.status,
+        };
+        return axiosErrorResult;
+      }
+
+      const clientErrorResult: RegisterUserResponse = {
+        error: true,
+      };
+
+      return clientErrorResult;
+    }
+  };
+
+  /**
+   * The function `getAllUsers` asynchronously fetches user authentication data and updates the `users`
+   * value with the response.
+   */
+  const getAllUsers = async (): Promise<void> => {
+    try {
+      isLoading.value = true;
+
+      const response: UserAuthData[] | null = await getAllUsersService();
+
+      users.value = response;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  /**
+   * The function `deleteUser` is an asynchronous function that deletes a user by calling the
+   * `deleteUserService` function with the provided `id`, handling any errors that may occur.
+   * @param {string} id - The `id` parameter in the `deleteUser` function is a string that represents
+   * the unique identifier of the user that you want to delete.
+   */
+  const deleteUser = async (id: UserAuthData['_id']): Promise<void> => {
+    try {
+      isLoading.value = true;
+      await deleteUserService(id);
+      await getAllUsers();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return {
+    users,
+    userAuthData,
+    getAllUsers,
+    deleteUser,
+    isLoading,
+    register,
+  };
+});
---
+++ src/stores/usersStore.spec.ts
@@ -0,0 +1,195 @@
+import { createTestingPinia } from '@pinia/testing';
+
+import { useUsersStore } from './users';
+import { createRandomUser } from '@/utils/mocks/user/mockUser';
+import * as UsersWebservice from '@/webservices/users/UsersWebservice';
+import { createRandomUsersList } from '@/utils/mocks/user/mockUsers';
+import { RegisterUserResponse } from '@/models/auth/registerUser';
+
+jest.mock('@/webservices/users/UsersWebservice');
+jest.mock('vue-i18n', () => ({
+  useI18n: () => ({
+    t: (key: string) => key,
+  }),
+}));
+jest.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+const deleteUserServiceMock = jest.spyOn(UsersWebservice, 'deleteUserService');
+const getAllUsersServiceMock = jest.spyOn(
+  UsersWebservice,
+  'getAllUsersService'
+);
+const registerServiceMock = jest.spyOn(UsersWebservice, 'registerService');
+
+const userMock = createRandomUser();
+const mockUsers = createRandomUsersList();
+
+describe('01 Auth store: getAllUsers', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    window.localStorage.clear();
+  });
+  it('01 - 01 Should get all user data', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    getAllUsersServiceMock.mockResolvedValue(mockUsers);
+
+    await usersStore.getAllUsers();
+
+    expect(usersStore.users).toStrictEqual(mockUsers);
+  });
+  it('01 - 02 Should get all user data error', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    getAllUsersServiceMock.mockRejectedValue(new Error('Login Failed'));
+    const consoleErrorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+
+    await usersStore.getAllUsers();
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.any(Error));
+    expect(usersStore.users).toStrictEqual([]);
+  });
+  it('01 - 03 Should get all user data null', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    getAllUsersServiceMock.mockRejectedValue(
+      createRandomUsersList({ nullUser: true })
+    );
+
+    await usersStore.getAllUsers();
+
+    expect(usersStore.users).toStrictEqual([]);
+  });
+});
+
+describe('02 Auth store: register', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('02 - 01 Should register', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+    const mockUser = createRandomUser();
+
+    registerServiceMock.mockResolvedValue(mockUser);
+
+    const expectedResponse = {
+      error: false,
+      result: mockUser,
+    };
+
+    const response: RegisterUserResponse = await usersStore.register(mockUser);
+
+    expect(response).toStrictEqual(expectedResponse);
+  });
+
+  it('02 - 02 Should handle network error', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    registerServiceMock.mockRejectedValue(new Error('Network error'));
+
+    const expectedResponse = {
+      error: true,
+    };
+
+    const response: RegisterUserResponse = await usersStore.register(userMock);
+
+    expect(response).toStrictEqual(expectedResponse);
+  });
+
+  it('02 - 03 Should handle 500 Internal Server Error', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    registerServiceMock.mockRejectedValue({
+      isAxiosError: true,
+      response: { status: 500 },
+    });
+
+    const expectedResponse = {
+      error: true,
+      status: 500,
+    };
+
+    const response: RegisterUserResponse = await usersStore.register(userMock);
+
+    expect(response).toStrictEqual(expectedResponse);
+  });
+});
+
+describe('03 Auth store: delete User', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    window.localStorage.clear();
+  });
+  it('03 - 01 Should delete a user', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    getAllUsersServiceMock.mockResolvedValue(mockUsers);
+
+    await usersStore.getAllUsers();
+
+    expect(usersStore.users).toStrictEqual(mockUsers);
+
+    deleteUserServiceMock.mockResolvedValue(undefined);
+
+    await usersStore.deleteUser(mockUsers![0]._id);
+
+    expect(usersStore.users).not.toContain(mockUsers![0]._id);
+  });
+
+  it('03 - 02 fail delete a user', async () => {
+    const pinia = createTestingPinia({
+      // Example of other aproach to pinia testing
+      stubActions: false,
+    });
+    const usersStore = useUsersStore(pinia);
+
+    getAllUsersServiceMock.mockResolvedValue(mockUsers);
+
+    await usersStore.getAllUsers();
+
+    expect(usersStore.users).toStrictEqual(mockUsers);
+
+    deleteUserServiceMock.mockRejectedValue(new Error('Login Failed'));
+    const consoleErrorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {});
+
+    await usersStore.deleteUser(mockUsers![0]._id);
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.any(Error));
+  });
+});
