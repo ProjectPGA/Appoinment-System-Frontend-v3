e3a3f6f74ae907d94f6743258727ee9009b531f1
gonzalo-plaza <plazaruedag@gmail.com>
2024-05-08 18:11:17 +0200

    [AS-82] utils: Add JSDoc to getJsonHeaders function and improve the return type

--- src/webservices/utils.ts
+++ src/webservices/utils.ts
@@ -1,7 +1,19 @@
-export const getJsonHeaders: (throwGlobalErrors: boolean) => object = (
+import { JsonHeaders } from './models/http/JsonHeaders';
+
+/**
+ * The function `getJsonHeaders` returns an object with headers for JSON content and an option to throw
+ * global errors.
+ *
+ * @param [throwGlobalErrors=false] - The `throwGlobalErrors` parameter is a boolean flag that
+ * determines whether global errors should be thrown or not. When set to `true`, global errors will be
+ * thrown.
+ *
+ * @returns An object of type JsonHeaders is being returned
+ */
+export const getJsonHeaders: (throwGlobalErrors: boolean) => JsonHeaders = (
   throwGlobalErrors = false
 ) => {
-  return {
+  const jsonHeaders: JsonHeaders = {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -9,4 +21,5 @@ export const getJsonHeaders: (throwGlobalErrors: boolean) => object = (
     withCredentials: true,
     throwGlobalErrors: throwGlobalErrors,
   };
+  return jsonHeaders;
 };
