3e758fd45f3a87c247481cbf17bbda6272067a72
gonzalo-plaza <plazaruedag@gmail.com>
2024-05-18 14:45:20 +0200

    [AS-82] ErrorHandler: Add new error codes and messages

--- src/webservices/models/http/ErrorHandler.ts
+++ src/webservices/models/http/ErrorHandler.ts
@@ -3,7 +3,27 @@ import { AxiosError, HttpStatusCode } from 'axios';
 export type THttpError = Error | AxiosError | null;
 
 export enum GlobalErrorHandlerMessages {
-  Unrecoverrable = 'Unrecoverrable error!! Error is null!',
+  Unrecoverrable = 'Unrecoverable error!! Error is null!',
+  BadRequest = 'The server could not understand the request due to invalid syntax.',
+  Unauthorized = 'You are not authorized to access this resource. Please authenticate yourself.',
+  Forbidden = 'Access to the requested resource is forbidden.',
+  NotFound = 'The requested resource could not be found on the server.',
+  RequestTimeout = 'The server timed out waiting for the request.',
+  TooManyRequests = 'You have exceeded the rate limit. Please try again later.',
+  InternalServerError = 'The server encountered an internal error and was unable to complete your request.',
+  BadGateway = 'The server received an invalid response from the upstream server.',
+  ServiceUnavailable = 'The server is currently unavailable. Please try again later.',
+  GatewayTimeout = 'The server did not receive a timely response from the upstream server.',
+  ECONNABORTED = 'The request was aborted due to a timeout.',
+  ERR_NETWORK = 'A network error occurred while making the request.',
+  ERR_INVALID_URL = 'The URL provided for the request is invalid.',
+  ERR_BAD_REQUEST = 'The request to the server is invalid or malformed.',
+  ERR_DEPRECATED = 'The functionality used in the request is deprecated and no longer supported. Please update your code.',
+  ERR_BAD_OPTION_VALUE = 'The value provided for an option in the request is invalid.',
+  ERR_BAD_OPTION = 'An invalid option was provided in the request.',
+  ERR_BAD_RESPONSE = 'The response received from the server is invalid or malformed.',
+  ERR_CANCELED = 'The request was canceled before completion.',
+  ETIMEDOUT = 'The request timed out before receiving a response.',
 }
 
 export interface ErrorHandlerObject {
@@ -23,16 +43,27 @@ export interface ErrorHandlerMany {
 }
 
 export const globalErrorHandlers: ErrorHandlerMany = {
-  [HttpStatusCode.BadRequest]: { message: 'Bad Request' },
-  [HttpStatusCode.Unauthorized]: {
-    message: 'Unauthorized!',
-  },
-  [HttpStatusCode.Forbidden]: { message: 'Forbidden' },
-  [HttpStatusCode.NotFound]: { message: 'Not Found' },
-  [HttpStatusCode.RequestTimeout]: { message: 'Request Timeout' },
-  [HttpStatusCode.TooManyRequests]: { message: 'Too Many Requests' },
-  [HttpStatusCode.InternalServerError]: { message: 'Internal Server Error' },
-  [HttpStatusCode.BadGateway]: { message: 'Bad Gateway' },
-  [HttpStatusCode.ServiceUnavailable]: { message: 'Service Unavailable' },
-  [HttpStatusCode.GatewayTimeout]: { message: 'Gateway Timeout' },
+  [HttpStatusCode.BadRequest]: GlobalErrorHandlerMessages.BadRequest,
+  [HttpStatusCode.Unauthorized]: GlobalErrorHandlerMessages.Unauthorized,
+  [HttpStatusCode.Forbidden]: GlobalErrorHandlerMessages.Forbidden,
+  [HttpStatusCode.NotFound]: GlobalErrorHandlerMessages.NotFound,
+  [HttpStatusCode.RequestTimeout]: GlobalErrorHandlerMessages.RequestTimeout,
+  [HttpStatusCode.TooManyRequests]: GlobalErrorHandlerMessages.TooManyRequests,
+  [HttpStatusCode.InternalServerError]:
+    GlobalErrorHandlerMessages.InternalServerError,
+  [HttpStatusCode.BadGateway]: GlobalErrorHandlerMessages.BadGateway,
+  [HttpStatusCode.ServiceUnavailable]:
+    GlobalErrorHandlerMessages.ServiceUnavailable,
+  [HttpStatusCode.GatewayTimeout]: GlobalErrorHandlerMessages.GatewayTimeout,
+  [AxiosError.ECONNABORTED]: GlobalErrorHandlerMessages.ECONNABORTED,
+  [AxiosError.ERR_NETWORK]: GlobalErrorHandlerMessages.ERR_NETWORK,
+  [AxiosError.ERR_INVALID_URL]: GlobalErrorHandlerMessages.ERR_INVALID_URL,
+  [AxiosError.ERR_BAD_REQUEST]: GlobalErrorHandlerMessages.ERR_BAD_REQUEST,
+  [AxiosError.ERR_DEPRECATED]: GlobalErrorHandlerMessages.ERR_DEPRECATED,
+  [AxiosError.ERR_BAD_OPTION_VALUE]:
+    GlobalErrorHandlerMessages.ERR_BAD_OPTION_VALUE,
+  [AxiosError.ERR_BAD_OPTION]: GlobalErrorHandlerMessages.ERR_BAD_OPTION,
+  [AxiosError.ERR_BAD_RESPONSE]: GlobalErrorHandlerMessages.ERR_BAD_RESPONSE,
+  [AxiosError.ERR_CANCELED]: GlobalErrorHandlerMessages.ERR_CANCELED,
+  [AxiosError.ETIMEDOUT]: GlobalErrorHandlerMessages.ETIMEDOUT,
 };
