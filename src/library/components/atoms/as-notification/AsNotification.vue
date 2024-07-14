a49adca56701921e6ccc6c27eaba56ee16d147dd
gonzalo-plaza <plazaruedag@gmail.com>
2024-07-14 13:53:36 +0200

    [AS-84] AsNotification.vue: Create component in components library

---
+++ src/library/components/atoms/as-notification/AsNotification.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="as-notification" :class="{ 'is-small': $props.isSmall }">
+    <span v-show="!isSmall">{{ props.quantity }}</span>
+  </div>
+</template>
+<script lang="ts" setup>
+import './as-notification.scss';
+
+const props = defineProps<{
+  /**
+   * size of the notification
+   */
+  isSmall?: boolean;
+  /**
+   * Quantity to show
+   */
+  quantity?: number;
+}>();
+</script>
