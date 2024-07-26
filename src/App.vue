66b6c455a342549a1acf4de65b3c2e7e625dfea6
gonzalo-plaza <plazaruedag@gmail.com>
2024-07-26 18:22:02 +0200

    [AS-84] App.vue: Use main-layout component when the user is logged.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <RouterView />
+  <main-layout v-if="authStore.isLogged">
+    <RouterView />
+  </main-layout>
+  <RouterView v-else />
   <loading-component v-if="isLoading" />
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue';
 import LoadingComponent from '@/components/common/LoadingComponent.vue';
+import MainLayout from './components/Layouts/MainLayout/MainLayout.vue';
 
 import { useAuthStore } from '@/stores/auth';
 
