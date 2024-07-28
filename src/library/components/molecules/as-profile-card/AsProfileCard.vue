db2077178b32d44e5eeda1d090abfc1592df8f0f
gonzalo-plaza <plazaruedag@gmail.com>
2024-07-28 20:03:10 +0200

    [AS-84] AsProfileCard.vue: Use picture html label to load profile image

--- src/library/components/molecules/as-profile-card/AsProfileCard.vue
+++ src/library/components/molecules/as-profile-card/AsProfileCard.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="as-profile-card" :class="{ 'is-active': props.active }">
-    <img
-      title="Profile image"
-      width="54"
-      height="54"
-      alt="User profile image"
-      :src="props.profileImageUrl ?? '/img/default-profile-image.webp'"
-      class="as-profile-card__image"
-    />
+    <picture>
+      <source
+        media="(max-width:1023px)"
+        width="40"
+        height="40"
+        :srcset="
+          props.profileImageUrlSmall ?? '/img/default-profile-image-40.webp'
+        "
+      />
+      <source
+        media="(min-width:1024px)"
+        width="54"
+        height="54"
+        :srcset="props.profileImageUrl ?? '/img/default-profile-image.webp'"
+      />
+      <img
+        class="as-profile-card__image"
+        :src="props.profileImageUrl ?? '/img/default-profile-image.webp'"
+        width="54"
+        height="54"
+        title="Profile image"
+        alt="User profile image"
+      />
+    </picture>
     <div class="as-profile-card__user-info">
       <p class="as-profile-card__user-name">
         {{ `${props.userName} ${props.userSurname}` }}
@@ -25,6 +41,10 @@ const props = withDefaults(
      * Url of user profile image
      */
     profileImageUrl?: string;
+    /**
+     * Url of user profile image
+     */
+    profileImageUrlSmall?: string;
     /**
      * User Name
      */
@@ -44,6 +64,7 @@ const props = withDefaults(
   }>(),
   {
     profileImageUrl: '/img/default-profile-image.webp',
+    profileImageUrlSmall: '/img/default-profile-image-40.webp',
     userName: 'Name',
     userSurname: 'Surname',
     userEmail: 'email@email.com',
